fbbb4ef9eaaeb6a70c6aaca1dde8ae4868df1f91
Avermakov <avermakov@list.ru>
2024-04-08 14:53:48 +0600

    Allow 8 XXX XXX XX XX phone numbers

--- src/app/static/app/js/common.js
+++ src/app/static/app/js/common.js
@@ -6,3 +6,13 @@ document.showNotification = function(message){
     setTimeout(()=>{notificationDom.style.opacity = 0}, 2500)
     setTimeout(()=>{notificationDom.style.display = 'none'}, 2750)
 }
+
+// Phonenumber fix
+const phoneNumberField = document.querySelector('#id_phone_number')
+const submitButton = document.querySelector('button[type="submit"]')
+
+submitButton.addEventListener('click', ()=>{
+    if (phoneNumberField.value.startsWith('8')){
+        phoneNumberField.value = `+7${phoneNumberField.value.slice(1)}`
+    } 
+})
