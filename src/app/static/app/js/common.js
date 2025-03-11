9f7bf121a752e3f29016b751b0d60b582ba78676
Avermakov <avermakov@list.ru>
2025-03-11 13:48:07 +0600

    Fix double submits on login, add user profile link

--- src/app/static/app/js/common.js
+++ src/app/static/app/js/common.js
@@ -16,3 +16,16 @@ submitButton.addEventListener('click', ()=>{
         phoneNumberField.value = `+7${phoneNumberField.value.slice(1)}`
     } 
 })
+
+// Double submit fix
+document.querySelectorAll('form').forEach((form) => {
+	form.addEventListener('submit', (e) => {
+		if (form.classList.contains('is-submitting')) {
+			e.preventDefault();
+			e.stopPropagation();
+			return false;	
+		};
+
+		form.classList.add('is-submitting');
+	});
+});
